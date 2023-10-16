f37b2f4ddd74cc371cfcad0ca9d8c20c81003d2c
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-10-16 20:06:55 +0100

    edited as checker has increased the Reds :smile

--- models/user.py
+++ models/user.py
@@ -10,15 +10,15 @@ class User(BaseModel):
         defines the User class
     '''
 
-    def __init__(self, *args, **kwargs):
+    # def __init__(self, *args, **kwargs):
         '''
             defined the public class attrib;
                 to be  called when a new instance of
                 the class User is created.
         '''
-        super().__init__(*args, **kwargs)
+        # super().__init__(*args, **kwargs)
 
-        self.email = ''
-        self.password = ''
-        self.first_name = ''
-        self.last_name = ''
+    self.email = ''
+    self.password = ''
+    self.first_name = ''
+    self.last_name = ''
