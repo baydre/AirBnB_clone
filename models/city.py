304eb2bd93ad1ff404680e8447e8de216dba7555
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-10-16 09:11:16 +0100

    the city class model

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+    The City class
+'''
+from models.base_model import BaseModel
+
+
+class City:
+    '''
+        defines the City class
+    '''
+    state_id = ''
+    name = ''
