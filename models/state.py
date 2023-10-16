847fbd219e8fbe14fac33717f2e2e997871891e0
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-10-16 08:21:46 +0100

    the state class model

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+    A State class that inherits from the BaseModel class
+'''
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    '''
+        defines the State class
+    '''
+    name = ''
