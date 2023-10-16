1074c0186201ca0499b06ce8d46de549469eb3a7
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-10-16 05:15:05 +0100

    bug fixes

--- models/base_model.py
+++ models/base_model.py
@@ -20,8 +20,9 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         '''
             initialise values
+            Re-created an instance with
+            dictionary representation
         '''
-        # Re-created an instance with dictionary representation
         format = '%Y-%m-%dT%H:%M:%S.%f'
         if kwargs:
             for key, value in kwargs.items():
@@ -33,7 +34,10 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            # create a unique FileStorage instance for your application
+            '''
+                create a unique FileStorage instance
+                for your application
+            '''
             models.storage.new(self)
 
     def __str__(self):
@@ -46,7 +50,9 @@ class BaseModel():
         return ("[{}] ({}) {}".format(type(self).__name__,
                 self.id, self.__dict__))
 
-    # Public instance methods
+    '''
+        Public instance methods
+    '''
     def save(self):
         '''
             updates the public instances attrib. update_at
@@ -58,7 +64,8 @@ class BaseModel():
     def to_dict(self):
         '''
             Return:
-                dictionary containing keys/values of __dict__ of instance
+                dictionary containing keys/values
+                of __dict__ of instance
         '''
         my_dict = dict(self.__dict__)
         my_dict['__class__'] = self.__class__.__name__
