30d573bb789b0fd0e00e1f6a11a664ffa1f484a0
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-10-16 09:14:39 +0100

    The amenity class model

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+    An Amenity class that inherits from BaseModel
+'''
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    '''
+        Defines the Amenity class
+    '''
+    name = ''
