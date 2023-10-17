a29c0ca4c6650de32d51244867ed688a154b400c
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-10-17 11:38:15 +0100

    the review class model

--- models/review.py
+++ models/review.py
@@ -3,20 +3,24 @@
     the review class model
 '''
 from models.base_model import BaseModel
-from models.place import Place
-from models.user import User
+# from models.place import Place
+# from models.user import User
 
 
 class Review(BaseModel):
     '''
         define the Review class
     '''
+    place_id = ''
+    user_id = ''
+    text = ''
 
-
+    '''
     def __init__(self, *args, **kwargs):
-        '''
+        ''
             method
-        '''
+        ''
         self.place_id = Place.id
         self.user_id = User.id
-        self.test = ''
+        self.text = ''
+    '''
