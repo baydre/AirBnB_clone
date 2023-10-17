9f23427e84b134db671f6f4e642520040eeedb75
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-10-17 12:16:42 +0100

    updated file storage to manage correctly serializtion and deserialization of all new classes: Place, State, City, Amenity and review

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,11 @@
 import models
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.City import city
+from models.amenity import Amenity
+from models.review import Review
 import json
 from os.path import isfile
 
