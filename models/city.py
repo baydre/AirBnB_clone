822fbeeec07e6dc8ed4716a0fcc89ef7efada867
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-10-17 11:39:03 +0100

    the city class model

--- models/city.py
+++ models/city.py
@@ -3,19 +3,21 @@
     The City class
 '''
 from models.base_model import BaseModel
-from models.state import State
+# from models.state import State
 
 
-class City:
+class City(BaseModel):
     '''
         defines the City class
     '''
     state_id = ''
     name = ''
-
+    
+    '''
     def __init__(self, state_id, name):
-        '''
+        ''
             attributes: public method
-        '''
+        ''
         self.state_id = State.id
         self.name = ''
+    '''
